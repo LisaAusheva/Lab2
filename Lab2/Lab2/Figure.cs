51f618390d070228ed534cc942ba30a5240837d5
LisaAusheva <43342322+LisaAusheva@users.noreply.github.com>
2018-12-25 22:59:41 +0300

    Add files via upload

---
+++ Lab2/Lab2/Figure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+   abstract partial class Figure
+    {
+        private string _Type;
+        /// <summary>
+        /// Название фигуры
+        /// </summary>
+        public string Type
+        {
+            get { return this._Type; }
+            set { this._Type = value; }
+        }
+        /// <summary>
+        /// Вычисление площади
+        /// </summary>
+        /// <returns></returns>
+        abstract public double Area();
+
+        public override string ToString()
+        {
+            Console.WriteLine(this.Type + ":");
+            return this.Type + " с площадью " + this.Area().ToString();
+            
+        }
+
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle R = new Rectangle(20, 4);
+            Square S = new Square(5.41231);
+            Circle C = new Circle(3.14);
+
+            R.Print();
+            S.Print();
+            C.Print();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
